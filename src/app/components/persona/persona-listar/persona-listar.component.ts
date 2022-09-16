f44ba7b5de2fb2df672e7bc570f5fbb8fe8c3403
Roxana Fernández <roxana.jacquelinefa@fpuna.edu.py>
2022-09-16 01:43:35 -0400

    Persona: Se implementó FILTRADO

--- src/app/components/persona/persona-listar/persona-listar.component.ts
+++ src/app/components/persona/persona-listar/persona-listar.component.ts
@@ -10,6 +10,18 @@ import { PersonaService } from 'src/app/services/persona.service';
 export class PersonaListarComponent implements OnInit {
   personas: Persona[] = [];
   mensaje!: string;
+  ordenes = [
+    { id: "asc", value: "ascendente" },
+    { id: "desc", value: "descendente" },
+  ]
+  atributos = [
+    { value: "nombre" },
+    { value: "apellido" }
+  ]
+  orden!: string;
+  atributo!: string;
+  nombreCompleto!: string;
+  nombreContiene!: string;
 
   constructor(private servicioPersonas: PersonaService) { }
 
@@ -17,15 +29,48 @@ export class PersonaListarComponent implements OnInit {
     this.getPersonas();
   }
 
-  getPersonas(queryParams: {}={}){
-    this.mensaje="";
-    this.servicioPersonas.getPersonas({like: 'S', ejemplo: JSON.stringify(queryParams)}).subscribe({
+  getPersonas(queryParams: {} = {}) {
+    this.mensaje = "";
+    this.servicioPersonas.getPersonas(queryParams).subscribe({
       next: (entity) => {
-        this.personas = entity.lista; 
-        if(this.personas.length==0)
-          this.mensaje="No hay resultados." 
+        this.personas = entity.lista;
+        if (this.personas.length == 0)
+          this.mensaje = "No hay resultados."
       },
       error: (e) => this.mensaje = e
     });
+  }
+
+  filtrarPersonas() {
+    let params: any = {};
+    if (this.orden != undefined && this.orden != null)
+      params["orderDir"] = this.orden;
+    if (this.atributo != null && this.atributo != undefined) {
+      params["orderBy"] = this.atributo;
+    }
+    this.getPersonas(params);
+  }
+
+  buscarPorNombre() {
+    let params: any = {};
+    if (this.nombreCompleto != undefined && this.nombreCompleto != null){
+      params["ejemplo"] =  JSON.stringify({"nombre": this.nombreCompleto}) ;
+      this.getPersonas(params);
+    }
+  }
+
+  buscarSiContiene() {
+    let params: any = {};
+    if (this.nombreContiene != undefined && this.nombreContiene != null){
+      params["like"] = 'S';
+      params["ejemplo"] =  JSON.stringify({"nombre": this.nombreContiene}) ;
+      this.getPersonas(params);
+    }
+  }
+
+  mostrarUsuariosSistema() {
+    let params: any = {};
+    params["ejemplo"] =  JSON.stringify({"soloUsuariosDelSistema": true});
+    this.getPersonas(params);
   }
 }
